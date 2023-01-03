7a9776e74ef9302adeef1d23f5790fc65f739ebe
Herzon Tan <herztan15@gmail.com>
2023-01-03 11:57:38 +0800

    debug 9

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ app.post("/test", (req, res) => {
 })
 
 app.get("/test", (req, res) => {
-    // const userOtp = '9999';
-    // const dialogflowReq = "{\"responseId\":\"054ab2fc-2f77-404f-b872-9c9ae9239758-c4fd3c84\",\"queryResult\":{\"queryText\":\"loremipsumab123459999\",\"parameters\":{\"policy_no\":\"ab12345\",\"otp\":\"9999\"},\"allRequiredParamsPresent\":true,\"outputContexts\":[{\"name\":\"projects/sunshine-uxpm/agent/sessions/e96d4eea-87da-eccc-0c08-96eb5bbd6749/contexts/__system_counters__\",\"parameters\":{\"no-input\":0,\"no-match\":0,\"policy_no\":\"ab12345\",\"policy_no.original\":\"ab12345\",\"otp\":\"9999\",\"otp.original\":\"9999\"}}],\"intent\":{\"name\":\"projects/sunshine-uxpm/agent/intents/9661b4ca-3545-40fd-b053-602b9ab4b21f\",\"displayName\":\"Test\"},\"intentDetectionConfidence\":1,\"languageCode\":\"en\",\"sentimentAnalysisResult\":{\"queryTextSentiment\":{\"score\":0.4,\"magnitude\":0.4}}},\"originalDetectIntentRequest\":{\"source\":\"DIALOGFLOW_CONSOLE\",\"payload\":{}},\"session\":\"projects/sunshine-uxpm/agent/sessions/e96d4eea-87da-eccc-0c08-96eb5bbd6749\"}";
+    const userOtp = '9999';
+    const dialogflowReq = "{\"responseId\":\"054ab2fc-2f77-404f-b872-9c9ae9239758-c4fd3c84\",\"queryResult\":{\"queryText\":\"loremipsumab123459999\",\"parameters\":{\"policy_no\":\"ab12345\",\"otp\":\"9999\"},\"allRequiredParamsPresent\":true,\"outputContexts\":[{\"name\":\"projects/sunshine-uxpm/agent/sessions/e96d4eea-87da-eccc-0c08-96eb5bbd6749/contexts/__system_counters__\",\"parameters\":{\"no-input\":0,\"no-match\":0,\"policy_no\":\"ab12345\",\"policy_no.original\":\"ab12345\",\"otp\":\"9999\",\"otp.original\":\"9999\"}}],\"intent\":{\"name\":\"projects/sunshine-uxpm/agent/intents/9661b4ca-3545-40fd-b053-602b9ab4b21f\",\"displayName\":\"Test\"},\"intentDetectionConfidence\":1,\"languageCode\":\"en\",\"sentimentAnalysisResult\":{\"queryTextSentiment\":{\"score\":0.4,\"magnitude\":0.4}}},\"originalDetectIntentRequest\":{\"source\":\"DIALOGFLOW_CONSOLE\",\"payload\":{}},\"session\":\"projects/sunshine-uxpm/agent/sessions/e96d4eea-87da-eccc-0c08-96eb5bbd6749\"}";
 
     // const policyNo = dialogflowReq.queryResult.parameters.policy_no;
     // const otp = dialogflowReq.queryResult.parameters.otp;
@@ -62,7 +62,7 @@ app.get("/test", (req, res) => {
     //     response = 'Wrong otp';
     // }
 
-    res.json("heyyy");
+    res.json(dialogflowReq);
 
     // res.json({
     //     "fulfillmentMessages": [

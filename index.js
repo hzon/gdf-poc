f082932cce4e0f7cef05e09f96260f0d72fd690a
Herzon Tan <herztan15@gmail.com>
2023-01-03 10:45:04 +0800

    edited index.js

--- index.js
+++ index.js
@@ -9,6 +9,37 @@ app.use(express.json());
 // Routes
 app.use("/home", home);
 
+app.post("/test", (req, res) => {
+    const userOtp = '9999';
+
+    const policyNo = req.query.policy_no;
+    const otp = req.query.otp;
+
+    let newDate = new Date();
+    newDate.setTime(Date.now());
+    dateString = newDate.toUTCString();
+
+    let response = '';
+
+    if (otp === userOtp) {
+        response = 'Correct otp';
+    } else {
+        response = 'Wrong otp';
+    }
+
+    res.json({
+        "fulfillmentMessages": [
+            {
+                "text": {
+                    "text": [
+                        response
+                    ]
+                }
+            }
+        ]
+    });
+})
+
 // connection
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
